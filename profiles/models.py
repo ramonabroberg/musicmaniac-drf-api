41ed5c3ae38bfd75e292fa315f3b443e07abad42
ramonabroberg <>
2023-12-22 23:07:34 +0000

    add profilelist view, serializer and urls.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class Profile(models.Model):
@@ -9,7 +10,11 @@ class Profile(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default-picture_xzjf4x')
     website = models.URLField(blank=True)
+    created_at = models.DateTimeField(default=datetime(2023, 12, 22))
 
+    class Meta:
+        ordering: ['-created_at']
+    
     def __str__(self):
         return f"{self.user}'s profile"
 
