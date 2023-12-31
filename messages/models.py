94bf04913c8d5337138beed6134898db77a49715
ramonabroberg <>
2023-12-31 10:53:24 +0000

    add messages app, model and serializer

---
+++ messages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sent_messages'
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='received_messages'
+    )
+    content = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
