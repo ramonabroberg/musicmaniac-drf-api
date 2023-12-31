5c4a76b91f23fed17c7e15f8ad36756cb4206384
ramonabroberg <>
2023-12-31 11:25:44 +0000

    add message views

--- messages/views.py
+++ messages/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from messages.models import Message
+from messages.serializers import UserSerializer, MessageSerializer
 
-# Create your views here.
+class MesssageList(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(sender=user).order_by('-sent_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+
+class MessageConversation(generics.ListAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        other_user_id = self.kwargs['user_id']
+        return Message.objects.filter(
+            (models.Q(sender=user, receiver_id=other_user_id) | models.Q(
+                sender_id=other_user_id, receiver=user))
+        ).order_by('sent_at')
