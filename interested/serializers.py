efb691b82b8af49fb52cd58ca483b2b31f5bcc27
ramonabroberg <>
2024-01-01 23:52:51 +0000

    add interested serializer

--- interested/serializers.py
+++ interested/serializers.py
@@ -2,4 +2,16 @@ from rest_framework import serializers
 from interested.models import Interested
 
 class InterestedSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Interested
+        fields = ['id', 'created_at', 'owner', 'post']
     
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
