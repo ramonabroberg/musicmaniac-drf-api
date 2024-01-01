730fd62c46b9d41beeee0e32481f2966bc8fad94
ramonabroberg <>
2024-01-01 23:44:37 +0000

    migrate interested model

---
+++ interested/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+from interested.models import Interested
+
+class InterestedSerializer(serializers.ModelSerializer):
+    
--- messaging/serializers.py
+++ messaging/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from messaging.models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
