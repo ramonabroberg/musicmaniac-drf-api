631fdf56651183724dfd2f54463d5d6c436bba29
ramonabroberg <ramme96@gmail.com>
2024-03-11 08:36:22 +0000

    update allowed hosts and trusted origins

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,11 +62,16 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['musicmaniac-drf-api-961711dd9bd4.herokuapp.com',
-                 '8000-ramonabroberg-musicmania-crjonck118.us1.codeanyapp.com',
+ALLOWED_HOSTS = [
+   os.environ.get('ALLOWED_HOST'),
+   '8000-ramonabroberg-musicmania-crjonck118.us1.codeanyapp.com',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*', 'https://8000-ramonabroberg-musicmania-crjonck118.us1.codeanyapp.com']
+CSRF_TRUSTED_ORIGINS = [
+    'http://*',
+    'https://*',
+    'https://8000-ramonabroberg-musicmania-crjonck118.us1.codeanyapp.com'
+]
 
 # Application definition
 
