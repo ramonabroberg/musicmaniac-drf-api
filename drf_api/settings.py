632434cedcf9fe5fb7fc5bb52430075de5a291df
ramonabroberg <ramme96@gmail.com>
2024-05-14 08:53:10 +0000

    add bottom new line on files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -172,10 +172,12 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
